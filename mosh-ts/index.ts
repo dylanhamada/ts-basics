127d101f11c8282da0f120c710a721d83e071440
Dylan Hamada <dylan.hamada@gmail.com>
2024-04-12 13:35:39 -0400

    Remove ts-basics Directory

--- mosh-ts/index.ts
+++ mosh-ts/index.ts
@@ -1,2 +1,9 @@
+// create a new tsconfig.json file with the following command
+// tsc --init
+
+// in tsconfig.json, the "target" property should be set to whatever ECMASCRIPT standard is needed
+// the "rootDir" property should reference the target folder for .ts files to be compiled
+// typing the command tsc in the terminal will compile all TypeScript files in the main directory to JavaScript files
+
 // annotate the variable type
 let age: number = 20;
